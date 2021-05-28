759d2ed936acce3fd959858a4f2f6be8acd4beaa
nawodyaishan <www.navodya221@gmail.com>
2021-05-28 22:04:47 +0530

    deletion

--- js/custom.min.js
+++
@@ -1,6 +0,0 @@
-/*!
- * Start Bootstrap - Grayscale v5.0.9 (https://startbootstrap.com/template-overviews/grayscale)
- * Copyright 2013-2020 Start Bootstrap
- * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-grayscale/blob/master/LICENSE)
- */
-
